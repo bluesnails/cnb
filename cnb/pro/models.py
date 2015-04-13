99c8eb9d99fa60ba57562a153a5272dd292d9924
vijendra <vijkush14@gmail.com>
2015-04-13 02:59:02 -0400

    Implemented 3nf for tags, added a form handler view

--- cnb/pro/models.py
+++ cnb/pro/models.py
@@ -18,22 +18,30 @@ BLOOM_TAG = (
 	('APP': 'Application'),
 )
 
+QUES_TYPES = (
+	('0': 'Matching'),
+	('1': 'Single Choice Correct'),
+	('2': 'Two Choices Correct'),
+	('3': 'Three Choices Correct'),
+	('4': 'Four Choices Correct'),
+)
+
 class question(models.Model):
 
 	ques_id = models.AutoField(primary_key=True) 
 	ques_text = models.TextField(max_length=1024)
-	ques_auth = models.ForeignKey('author')
-	ques_doc = models.DateField(auto_add_now=True)
+	ques_author = models.ForeignKey('author')
+	ques_created = models.DateField(auto_add_now=True)
 	ques_dscore = models.IntegerField()
 	ques_bloom = models.CharField(max_length=3, choices=BLOOM_TAG)
 	ques_subject = models.CharField(max_length=3, choices=GS_SUBJECTS)
-	ques_atags = models.CharField(max_length=32)
-	ques_mtags = models.CharField(max_length=32)
+	ques_type = models.CharField(max_length=1, choices=QUES_TYPES)
+	ques_flags = models.CharField(max_length=16)
 
 	def __unicode__(self):
 		return self.ques_text
 
-class choices(models.Model):
+class choice(models.Model):
 
 	choice_id = models.AutoField(primary_key=True)
 	choice_text = models.CharField(max_length=128)
@@ -45,12 +53,40 @@ class choices(models.Model):
 		return self.choice_text
 
 class author(models.Model):
-
 	
-	author_name = models.CharField(max_length=32)
-	author_doj = models.DateField(auto_add_now=True)
-	author_org = models.CharField(max_length=32)
-	author_email = models.EmailField()
+	auth_id = models.AutoField(primary_key=True)	
+	auth_username = models.CharField(max_length=32)
+	auth_doj = models.DateField(auto_add_now=True)
+	auth_org = models.CharField(max_length=32)
+	auth_email = models.EmailField()
 
 	def __unicode__(self):
-		return self.author_name
+		return self.author_username
+
+class author_tags(models.Model):
+
+	atags_id = models.AutoField(primary_key=True)
+	atags_text = models.CharField(max_length=16)
+	
+	def __unicode__(self):
+		return self.atags_text
+
+class acon(models.Model):
+
+	acon_id = models.AutoField(primary_key=True)
+	acon_atag = models.ForeignKey('author_tags')
+	acon_ques = models.ForeignKey('question')
+
+class machine_tags(models.Model):
+
+	mtags_id = models.AutoField(primary_key=True)
+	mtags_text = models.CharField(max_length=16)
+
+	def __unicode__(self):
+		return self.mtags_text
+
+class mcon(models.Model):
+
+	mcon_id = models.AutoField(primary_key=True)
+	mcon_mtag = models.ForeignKey('machcine_tags')
+	mcon_ques = models.ForeignKey('qustion')
--- cnb/pro/views.py
+++ cnb/pro/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+
+from .models import question, choice
 
 def auth_staff(request):
     username = request.POST['username']
@@ -18,3 +21,29 @@ def auth_staff(request):
     else:
         # Return an 'invalid login' error message.
 
+@login_required(login_url='/accounts/login/')
+def add_ques(request):
+	return render('addquestion.html', context_instance=RequestContext(request))
+
+@login_required(login_url='/accounts/login/')
+def save_ques(request):
+
+	if request.method=='POST':
+		qtext = request.POST['ques_text']
+		qtype = request.POST['ques_type']
+		qdscore = request.POST['ques_dscore']
+		qbtag = request.POST['ques_btag']
+		qsub  = request.POST['ques_sub']
+		
+		ch1 = request.POST['ch1_text']
+		ch2 = request.POST['ch2_text']
+		ch3 = request.POST['ch3_text']
+		ch4 = request.POST['ch4_text']
+
+		Q = question(ques_text=qtext, ques_type=qtype, ques_dscore=qdscore, ques_bloom=qbtag, ques_subject=qsub)
+		ch1 = choice(choice_text=ch1_text, choice_ques=Q, choice_ans=ch1_ans)
+
+	else:
+		return HttpResponseRedirect(reverse('.views.add_ques'))
+
+	return render('saved.html', context_instance=RequestContext(request))
